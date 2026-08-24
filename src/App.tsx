daab0e1a19c647917ab77276a91156ad0987ec95
Baba Suleymanli <crestaze@gmail.com>
2026-08-23 20:15:36 -0700

    Add legal routes

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Footer } from "./components/Footer";
 import { Home } from "./pages/Home";
 import { Login } from "./pages/Login";
 import { BookDemo } from "./pages/BookDemo";
+import { Legal } from "./pages/Legal";
 import { NotFoundPage } from "./pages/NotFoundPage";
 
 function AppContent() {
@@ -20,6 +21,10 @@ function AppContent() {
           <Route path="/signup" element={<Login />} />
           <Route path="/book-demo" element={<BookDemo />} />
           <Route path="/demo" element={<BookDemo />} />
+          <Route path="/privacy" element={<Legal />} />
+          <Route path="/terms" element={<Legal />} />
+          <Route path="/cookies" element={<Legal />} />
+          <Route path="/security" element={<Legal />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </main>
